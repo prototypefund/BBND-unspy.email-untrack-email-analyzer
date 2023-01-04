4bb296a8fd8df7fcd92a2102ee74a34e7c1f55d2
Merlin <merlin@geeks4change.net>
2023-01-04 16:17:15 +0100

    Factor out PlainPhpRedirectResolver

--- src/RedirectResolver/AsyncGuzzleRedirectResolver.php
+++ src/RedirectResolver/AsyncGuzzleRedirectResolver.php
@@ -4,110 +4,25 @@ declare(strict_types=1);
 
 namespace Geeks4change\UntrackEmailAnalyzer\RedirectResolver;
 
-use Geeks4change\UntrackEmailAnalyzer\Analyzer\AnalyzerResult\Url;
 use Geeks4change\UntrackEmailAnalyzer\Analyzer\AnalyzerResult\UrlList;
 use Geeks4change\UntrackEmailAnalyzer\Analyzer\AnalyzerResult\UrlRedirectInfo;
 use Geeks4change\UntrackEmailAnalyzer\Analyzer\AnalyzerResult\UrlRedirectInfoList;
 use GuzzleHttp\Client;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Middleware;
-use GuzzleHttp\Promise\Each;
 use GuzzleHttp\Promise\Is;
-use GuzzleHttp\Promise\Promise;
-use GuzzleHttp\Promise\PromiseInterface;
 use GuzzleHttp\Promise\Utils;
-use GuzzleHttp\RequestOptions;
-use loophp\collection\Collection;
-use loophp\collection\Contract\Operation\Unwrapable;
 use Psr\Http\Message\ResponseInterface;
-use Symfony\Component\HttpClient\HttpClient;
-use function GuzzleHttp\Promise\unwrap;
 
+/**
+ * Async guzzle redirect resolver.
+ *
+ * ABANDONED, only for reference.
+ *
+ * This is fast. So fast, that APIS ban us. Throttling turned out to be not that
+ * easy.
+ */
 final class AsyncGuzzleRedirectResolver implements RedirectResolverInterface {
 
-  /**
-   * Resolve a redirect chain via plain PHP.
-   *
-   * Get redirect info **without** actually requesting the target. Why?
-   * - Some sites like LinkedIn serve invalid 999 status codes against crawling,
-   *   and it is not so easy to work around that.
-   * - Performing a full GET on an unsubscribe url often actually performs an
-   *   unsubscribe. While against the spec, this is what we got in the wild.
-   *
-   * Guzzle turned out bad at that, so using symfony http client.
-   */
-  public function resolveRedirect(string $url, $hops = 5): ?UrlRedirectInfo {
-
-
-    // Note that stream_context_create and passing the context has different
-    // result, as it breaks default crypto context.
-    stream_context_set_default(
-      [
-        'http' => [
-          'method' => 'HEAD',
-          'timeout' => 6,
-        ]
-      ]
-    );
-    $redirectUrls = [];
-    $currentUrl = $url;
-    do {
-      $headers = get_headers($currentUrl, TRUE);
-      if ($headers) {
-        $headers = self::normalizeHeaders($headers);
-        [, $statusCode] = explode(' ', $headers[0]);
-        $isRedirect = substr($statusCode, 0, 1) === '3';
-        $currentUrl = $isRedirect ? $this->extractLocation($headers) : NULL;
-      }
-      $isValidRedirect = !empty($isRedirect) && !empty($currentUrl);
-      if ($isValidRedirect) {
-        $redirectUrls[] = $currentUrl;
-      }
-    } while ($isValidRedirect);
-
-    return new UrlRedirectInfo($url, ...$redirectUrls);
-  }
-
-  protected static function normalizeHeaders(array $headers): array {
-    $keys = array_keys($headers);
-    $normalizedKeys = array_map('strtolower', $keys);
-    return array_combine($normalizedKeys, $headers);
-  }
-
-  public function extractLocation($headers): ?string {
-    // Either string or array of strings.
-    // Mailchimp does freaky things like this:
-    // 'location' => ['http://info@voeoe.de', 'https://voeoe.de/']
-    $locations = (array) ($headers['location'] ?? []);
-    foreach ($locations as $location) {
-      if (is_string($location) && preg_match('~https?://~ui', $location)) {
-        return $location;
-      }
-    }
-    return NULL;
-  }
-
-  /**
-   * Resolve redirections.
-   *
-   * @fixme Clean this up:
-   *   - Move everything to caller.
-   *   - Throttle async so it won't hit rate limits.
-   *   - Use $excludeUrlList
-   */
   public function resolveRedirects(UrlList $urlList): UrlRedirectInfoList {
-    $urlRedirectInfoList = new UrlRedirectInfoList();
-    foreach ($urlList as $urlWrapper) {
-      $url = $urlWrapper->toString();
-      $redirectInfo = $this->resolveRedirect($url);
-      if ($redirectInfo->hasRedirect()) {
-        $urlRedirectInfoList->add($redirectInfo);
-      }
-    }
-    return $urlRedirectInfoList;
-  }
-
-  public function resolveRedirectsAsync(UrlList $urlList): UrlRedirectInfoList {
     $client = new Client(['allow_redirects' => FALSE, 'timeout' => 8]);
     $promises = new \ArrayIterator();
     $redirectMap = [];
---
+++ src/RedirectResolver/PlainPhpRedirectResolver.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Geeks4change\UntrackEmailAnalyzer\RedirectResolver;
+
+use Geeks4change\UntrackEmailAnalyzer\Analyzer\AnalyzerResult\UrlList;
+use Geeks4change\UntrackEmailAnalyzer\Analyzer\AnalyzerResult\UrlRedirectInfo;
+use Geeks4change\UntrackEmailAnalyzer\Analyzer\AnalyzerResult\UrlRedirectInfoList;
+
+final class PlainPhpRedirectResolver {
+
+  public function resolveRedirects(UrlList $urlList): UrlRedirectInfoList {
+    $urlRedirectInfoList = new UrlRedirectInfoList();
+    foreach ($urlList as $urlWrapper) {
+      $url = $urlWrapper->toString();
+      $redirectInfo = $this->resolveRedirect($url);
+      if ($redirectInfo->hasRedirect()) {
+        $urlRedirectInfoList->add($redirectInfo);
+      }
+    }
+    return $urlRedirectInfoList;
+  }
+
+  /**
+   * Resolve a redirect chain via plain PHP.
+   *
+   * ABANDONED, only for reference.
+   *
+   * Successor of a plain guzzle redirect resolver, which turned out to be bad
+   * at the following.
+   *
+   * Get redirect info **without** actually requesting the target. Why?
+   * - Some sites like LinkedIn serve invalid 999 status codes against crawling,
+   *   and it is not so easy to work around that.
+   * - Performing a full GET on an unsubscribe url often actually performs an
+   *   unsubscribe. While against the spec, this is what we got in the wild.
+   */
+  public function resolveRedirect(string $url, $hops = 5): ?UrlRedirectInfo {
+    // Note that stream_context_create and passing the context has different
+    // result, as it breaks default crypto context.
+    stream_context_set_default(
+      [
+        'http' => [
+          'method' => 'HEAD',
+          'timeout' => 6,
+        ]
+      ]
+    );
+    $redirectUrls = [];
+    $currentUrl = $url;
+    do {
+      $headers = get_headers($currentUrl, TRUE);
+      if ($headers) {
+        $headers = self::normalizeHeaders($headers);
+        [, $statusCode] = explode(' ', $headers[0]);
+        $isRedirect = substr($statusCode, 0, 1) === '3';
+        $currentUrl = $isRedirect ? $this->extractLocation($headers) : NULL;
+      }
+      $isValidRedirect = !empty($isRedirect) && !empty($currentUrl);
+      if ($isValidRedirect) {
+        $redirectUrls[] = $currentUrl;
+      }
+    } while ($isValidRedirect);
+
+    return new UrlRedirectInfo($url, ...$redirectUrls);
+  }
+
+  protected static function normalizeHeaders(array $headers): array {
+    $keys = array_keys($headers);
+    $normalizedKeys = array_map('strtolower', $keys);
+    return array_combine($normalizedKeys, $headers);
+  }
+
+  public function extractLocation($headers): ?string {
+    // Either string or array of strings.
+    // Mailchimp does freaky things like this:
+    // 'location' => ['http://info@voeoe.de', 'https://voeoe.de/']
+    $locations = (array) ($headers['location'] ?? []);
+    foreach ($locations as $location) {
+      if (is_string($location) && preg_match('~https?://~ui', $location)) {
+        return $location;
+      }
+    }
+    return NULL;
+  }
+
+}

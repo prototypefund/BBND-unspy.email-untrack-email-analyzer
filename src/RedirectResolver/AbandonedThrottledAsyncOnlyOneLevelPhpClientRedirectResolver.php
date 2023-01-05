5d380eef52377c3c6fc5377559379b96de73a6bf
Merlin <merlin@geeks4change.net>
2023-01-05 12:09:20 +0100

    Fix symfony redirect detector

--- src/RedirectResolver/AbandonedThrottledAsyncOnlyOneLevelPhpClientRedirectResolver.php
+++ src/RedirectResolver/AbandonedThrottledAsyncOnlyOneLevelPhpClientRedirectResolver.php
@@ -7,11 +7,12 @@ namespace Geeks4change\UntrackEmailAnalyzer\RedirectResolver;
 use Geeks4change\UntrackEmailAnalyzer\Analyzer\AnalyzerResult\UrlList;
 use Geeks4change\UntrackEmailAnalyzer\Analyzer\AnalyzerResult\UrlRedirectInfo;
 use Geeks4change\UntrackEmailAnalyzer\Analyzer\AnalyzerResult\UrlRedirectInfoList;
+use Geeks4change\UntrackEmailAnalyzer\RedirectResolver\ClientDecorator\HttpClientDefaultHeaders;
 use Geeks4change\UntrackEmailAnalyzer\RedirectResolver\ClientDecorator\SimpleThrottlingHttpClient;
-use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpClient\NoPrivateNetworkHttpClient;
 use Symfony\Component\HttpClient\RetryableHttpClient;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 
 /**
  * Redirect resolver.
@@ -28,15 +29,15 @@ use Symfony\Component\HttpClient\RetryableHttpClient;
  */
 final class AbandonedThrottledAsyncOnlyOneLevelPhpClientRedirectResolver implements RedirectResolverInterface {
 
-  protected int $throttleMilliSeconds = 1000;
+  protected int $throttleMilliSeconds = 600;
 
   protected $client;
 
   public function __construct() {
-    // @fixme Add options.
     $this->client = HttpClient::create([
       'max_redirects' => 0,
       'timeout' => 8,
+      'headers' => HttpClientDefaultHeaders::get(),
     ]);
     // Prevent SSRF attacks.
     $this->client = new NoPrivateNetworkHttpClient($this->client);
@@ -52,7 +53,6 @@ final class AbandonedThrottledAsyncOnlyOneLevelPhpClientRedirectResolver impleme
     foreach ($urlList as $urlItem) {
       $url = $urlItem->toString();
       if (!isset($responses[$url])) {
-        dump("Request: $url");
         $responses[$url] = $this->client->request('GET', $url);
       }
     }
@@ -61,17 +61,17 @@ final class AbandonedThrottledAsyncOnlyOneLevelPhpClientRedirectResolver impleme
     foreach ($responses as $url => $response) {
       $redirectUrls = [];
       if ($redirectUrl = $this->extractRedirect($response)) {
-        $redirectUrls[] = $$redirectUrl;
+        $redirectUrls[] = $redirectUrl;
+        $urlRedirectInfo = new UrlRedirectInfo($url, ...$redirectUrls);
+        $urlRedirectInfoList->add($urlRedirectInfo);
       }
-      $urlRedirectInfo = new UrlRedirectInfo($url, ...$redirectUrls);
-      $urlRedirectInfoList->add($urlRedirectInfo);
     }
     return $urlRedirectInfoList;
   }
 
   protected function extractRedirect(ResponseInterface $response): ?string {
     if ($response->getStatusCode() >= 300 && $response->getStatusCode() < 400) {
-      return $response->getHeader('location')[0];
+      return $response->getHeaders(throw: false)['location'][0] ?? NULL;
     }
     else {
       return NULL;

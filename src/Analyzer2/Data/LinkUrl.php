dc7ff64c8609ede434b0d9c72ec429eec5201527
Merlin <merlin@geeks4change.net>
2023-02-15 22:48:55 +0100

    Fix UrlExtractor

--- src/Analyzer2/Data/LinkUrl.php
+++ src/Analyzer2/Data/LinkUrl.php
@@ -6,4 +6,13 @@ namespace Geeks4change\UntrackEmailAnalyzer\Analyzer2\Data;
 
 use Geeks4change\UntrackEmailAnalyzer\Analyzer2\Data\Url\UrlItem;
 
-final class LinkUrl extends UrlItem {}
+final class LinkUrl extends UrlItem {
+
+  public function __construct(
+                    string $url,
+    public readonly string $text,
+  ) {
+    parent::__construct($url);
+  }
+
+}
--- src/Analyzer2/UrlExtractor.php
+++ src/Analyzer2/UrlExtractor.php
@@ -8,7 +8,6 @@ use Geeks4change\UntrackEmailAnalyzer\Analyzer2\Data\ImageUrl;
 use Geeks4change\UntrackEmailAnalyzer\Analyzer2\Data\LinkUrl;
 use Geeks4change\UntrackEmailAnalyzer\Analyzer2\Data\Url\UrlItemBag;
 use Geeks4change\UntrackEmailAnalyzer\Analyzer2\Data\Url\UrlItemBagBuilder;
-use Geeks4change\UntrackEmailAnalyzer\Analyzer2\Data\Url\UrlItemType;
 use Symfony\Component\DomCrawler\Crawler;
 
 final class UrlExtractor {
@@ -17,10 +16,10 @@ final class UrlExtractor {
     $builder = new UrlItemBagBuilder();
     foreach ($crawler->filterXPath('//a[href]')->links() as $link) {
       // @todo Add link text.
-      $builder->addUrlItem(new LinkUrl(UrlItemType::Link, $link->getUri(), $link->getNode()->textContent));
+      $builder->addUrlItem(new LinkUrl($link->getUri(), $link->getNode()->textContent));
     }
     foreach ($crawler->filterXPath('//img[src]')->images() as $image) {
-      $builder->addUrlItem(new ImageUrl(UrlItemType::Image, $image->getUri()));
+      $builder->addUrlItem(new ImageUrl($image->getUri()));
     }
     return $builder->freeze();
   }

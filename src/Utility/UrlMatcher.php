0475869516b0266083ea84f2b99f915493879f8a
Merlin <merlin@geeks4change.net>
2023-02-17 11:53:00 +0100

    Document UrlMatcher

--- src/Utility/UrlMatcher.php
+++ src/Utility/UrlMatcher.php
@@ -27,21 +27,9 @@ final class UrlMatcher {
 
   public function match(string $uriString): bool {
     $uri = new Uri($uriString);
-
     $domainMatch = $this->hostOrCnameMatchesAnyDomain($uri->getHost());
-
-    $pathMatch = preg_match($this->pathRegexes, $uri->getPath());
-
-    parse_str($uri->getQuery(), $queryArray);
-    $matchesQueryKey = fn(?string $queryPattern, string | int $key) =>
-      isset($queryArray[$key])
-      && (
-        !$queryPattern ||
-        preg_match($queryPattern, $queryArray[$key])
-      );
-    $queryMatch = !$this->queryPatterns ||
-      Collection::fromIterable($this->queryPatterns)
-        ->every($matchesQueryKey);
+    $pathMatch = $this->pathMatchesPattern($uri->getPath());
+    $queryMatch = $this->queryMatchesPattern($uri->getQuery());
     return $domainMatch && $pathMatch && $queryMatch;
   }
 
@@ -64,21 +52,22 @@ final class UrlMatcher {
     return $isMatch;
   }
 
-  /**
-   * @param string $uriPattern
-   *   A pattern containing {} as placeholder with contextual semantics.
-   *   No scheme support.
-   */
-  public static function create(string $uriPattern, array $domains = []): UrlMatcher {
-    $uri = new Uri($uriPattern);
-    $domains = $uri->getHost() ? [$uri->getHost()] : $domains;
-    $pathPattern = self::createRegex($uri->getPath(), '?');
-    // Empty key (with or without '=') maps to empty string.
-    parse_str($uri->getQuery(), $queryArray);
-    $createQueryRegexes = fn(string $pattern) => $pattern ?
-      self::createRegex($pattern, '&') : NULL;
-    $queryPatterns = array_map($createQueryRegexes, $queryArray);
-    return new self($domains, $pathPattern, $queryPatterns);
+  public function pathMatchesPattern(string $path): bool {
+    return boolval(preg_match($this->pathRegexes, $path));
+  }
+
+  public function queryMatchesPattern(string $query): bool {
+    parse_str($query, $queryArray);
+    $matchesQueryKey = fn(?string $queryPattern, string|int $key) => isset($queryArray[$key])
+      && (
+        !$queryPattern ||
+        preg_match($queryPattern, $queryArray[$key])
+      );
+    /** @noinspection PhpUnnecessaryLocalVariableInspection */
+    $queryMatch = !$this->queryPatterns ||
+      Collection::fromIterable($this->queryPatterns)
+        ->every($matchesQueryKey);
+    return $queryMatch;
   }
 
   protected static function createRegex(string $pattern, $separator): string {
@@ -88,6 +77,34 @@ final class UrlMatcher {
     /** @noinspection PhpUnnecessaryLocalVariableInspection */
     $regex = "~^{$regexPart}($|[?]|[&]|#)~u";
     return $regex;
+  }
+
+  /**
+   * @param string $uriPattern
+   *   A pattern containing {} as placeholder with contextual semantics.
+   *     - If domain to match starts wit '.', any subdomain matches.
+   *     - Path may contain {} placeholders that match any nonempty path
+   *       segment (but not '/').
+   *     - Query matches like this:
+   *       - Must contain all specified keys.
+   *       - If a pattern is given, it is matched against query string value,
+   *         with {} matching any nonempty string.
+   * @param array $domains
+   *   If $domains is given, domain part of $uriPattern is ignored.
+   *
+   * @return \Geeks4change\UntrackEmailAnalyzer\Utility\UrlMatcher
+   */
+  public static function create(string $uriPattern, array $domains = []): UrlMatcher {
+    $uri = new Uri($uriPattern);
+    $domains = $uri->getHost() ? [$uri->getHost()] : $domains;
+    $pathPattern = self::createRegex($uri->getPath(), '?');
+    // Empty key (with or without '=') maps to empty string.
+    parse_str($uri->getQuery(), $queryArray);
+    $createQueryRegexes = fn(string $pattern) => $pattern ?
+      // No '&' separator necessary, query is parsed beforehand.
+      self::createRegex($pattern, '') : NULL;
+    $queryPatterns = array_map($createQueryRegexes, $queryArray);
+    return new self($domains, $pathPattern, $queryPatterns);
   }
 
 }
